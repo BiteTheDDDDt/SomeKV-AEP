0f0e43b6650c6ffbe89781aafc082090a155e5e0
BiteTheDDDDt <pxl290@qq.com>
2022-07-19 00:00:27 +0800

    update storage engine

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,81 +1,17 @@
 #include "interface.h"
 
-#include <string.h>
-
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-
 #include "storage/storage_engine.h"
 #include "storage/storage_engine_factory.h"
 
-class User {
-public:
-    int64_t id;
-    char user_id[128];
-    char name[128];
-    int64_t salary;
-};
-
-enum Column { Id = 0, Userid, Name, Salary };
-
-std::vector<User> users;
-
 void engine_write(void* ctx, const void* data, size_t len) {
-    User user;
-    memcpy(&user, data, len);
-    users.push_back(user);
+    auto* engine = static_cast<StorageEngine*>(ctx);
+    engine->write(data);
 }
 
 size_t engine_read(void* ctx, int32_t select_column, int32_t where_column, const void* column_key,
                    size_t column_key_len, void* res) {
-    int users_size = users.size();
-    bool b = true;
-    size_t res_num = 0;
-    res = malloc(128 * 2000);
-    for (int i = 0; i < users_size; ++i) {
-        switch (where_column) {
-        case Id:
-            b = memcmp(column_key, &users[i].id, column_key_len) == 0;
-            break;
-        case Userid:
-            b = memcmp(column_key, users[i].user_id, column_key_len) == 0;
-            break;
-        case Name:
-            b = memcmp(column_key, users[i].name, column_key_len) == 0;
-            break;
-        case Salary:
-            b = memcmp(column_key, &users[i].salary, column_key_len) == 0;
-            break;
-        default:
-            b = false;
-            break; // wrong
-        }
-        if (b) {
-            ++res_num;
-            switch (select_column) {
-            case Id:
-                memcpy(res, &users[i].id, 8);
-                res = (char*)res + 8;
-                break;
-            case Userid:
-                memcpy(res, users[i].user_id, 128);
-                res = (char*)res + 128;
-                break;
-            case Name:
-                memcpy(res, users[i].name, 128);
-                res = (char*)res + 128;
-                break;
-            case Salary:
-                memcpy(res, &users[i].salary, 8);
-                res = (char*)res + 8;
-                break;
-            default:
-                break; // wrong
-            }
-        }
-    }
-    return res_num;
+    auto* engine = static_cast<StorageEngine*>(ctx);
+    return engine->read(select_column, where_column, column_key, column_key_len, res);
 }
 
 void* engine_init(const char* host_info, const char* const* peer_host_info,
