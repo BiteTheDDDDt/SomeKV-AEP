a6bd402f354a845d02b1b3d800ea9be184df7462
BiteTheDDDDt <pxl290@qq.com>
2022-07-25 01:27:56 +0800

    update selector

--- src/storage/memory_storage.h
+++ src/storage/memory_storage.h
@@ -13,7 +13,9 @@ class MemoryStorage {
 public:
     void write(const Schema::Row* row_ptr) { _datas.emplace_back(*row_ptr); }
 
-    void update_selector(int32_t where_column, const void* column_key, size_t column_key_len) {
+    std::vector<uint8_t> update_selector(int32_t where_column, const void* column_key,
+                                         size_t column_key_len) {
+        std::vector<uint8_t> _selector;
         _selector.resize(_datas.size());
 
         if (where_column == Schema::Column::Id) {
@@ -45,16 +47,16 @@ public:
 
     size_t read(int32_t select_column, int32_t where_column, const void* column_key,
                 size_t column_key_len, void* res) {
-        update_selector(where_column, column_key, column_key_len);
+        auto selector = update_selector(where_column, column_key, column_key_len);
 
         size_t select_number = 0;
-        for (size_t i = 0; i < _selector.size(); i++) {
-            select_number += _selector[i];
+        for (size_t i = 0; i < selector.size(); i++) {
+            select_number += selector[i];
         }
 
         if (select_column == Schema::Column::Id) {
             for (size_t i = 0; i < _datas.size(); ++i) {
-                if (_selector[i]) {
+                if (selector[i]) {
                     memcpy(res, &_datas[i].id, Schema::ID_LENGTH);
                     res = (char*)res + Schema::ID_LENGTH;
                 }
@@ -63,7 +65,7 @@ public:
 
         if (select_column == Schema::Column::Salary) {
             for (size_t i = 0; i < _datas.size(); ++i) {
-                if (_selector[i]) {
+                if (selector[i]) {
                     memcpy(res, &_datas[i].salary, Schema::SALARY_LENGTH);
                     res = (char*)res + Schema::SALARY_LENGTH;
                 }
@@ -72,7 +74,7 @@ public:
 
         if (select_column == Schema::Column::Userid) {
             for (size_t i = 0; i < _datas.size(); ++i) {
-                if (_selector[i]) {
+                if (selector[i]) {
                     memcpy(res, &_datas[i].user_id, Schema::USERID_LENGTH);
                     res = (char*)res + Schema::USERID_LENGTH;
                 }
@@ -81,7 +83,7 @@ public:
 
         if (select_column == Schema::Column::Name) {
             for (size_t i = 0; i < _datas.size(); ++i) {
-                if (_selector[i]) {
+                if (selector[i]) {
                     memcpy(res, &_datas[i].name, Schema::NAME_LENGTH);
                     res = (char*)res + Schema::NAME_LENGTH;
                 }
@@ -95,5 +97,4 @@ public:
 
 private:
     std::vector<Schema::Row> _datas;
-    std::vector<uint8_t> _selector;
 };
