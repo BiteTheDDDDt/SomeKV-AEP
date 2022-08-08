c0b61b2613a68ca50242ede515a501b9be4e74df
BiteTheDDDDt <pxl290@qq.com>
2022-08-08 23:02:19 +0800

    update

--- src/io/pmem_file.h
+++ src/io/pmem_file.h
@@ -54,11 +54,11 @@ public:
     ~PmemFile() { pmem_unmap(_header, _mapped_len); }
 
     void append(const void* data_ptr) {
-        pmem_memcpy_nodrain(_current, data_ptr, Schema::ROW_LENGTH);
+        pmem_memcpy(_current, data_ptr, Schema::ROW_LENGTH, PMEM_F_MEM_NONTEMPORAL);
         _current += Schema::ROW_LENGTH;
 
         _size++;
-        pmem_memcpy_nodrain(_header, &_size, PMEM_HEADER_SIZE);
+        pmem_memcpy(_header, &_size, PMEM_HEADER_SIZE, PMEM_F_MEM_NONTEMPORAL);
     }
 
 private:
