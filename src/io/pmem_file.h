e33b0f6b8ffed6c8a4a53c8ee8cabc29f0f81cca
BiteTheDDDDt <pxl290@qq.com>
2022-08-20 15:23:26 +0800

    update

--- src/io/pmem_file.h
+++ src/io/pmem_file.h
@@ -58,7 +58,7 @@ public:
     void append(const void* data_ptr) {
         memcpy(_buffer + PMEM_HEADER_SIZE, data_ptr, Schema::ROW_LENGTH);
         pmem_memcpy(_current, _buffer, Schema::ROW_LENGTH + PMEM_HEADER_SIZE,
-                    PMEM_F_MEM_NONTEMPORAL);
+                    PMEM_F_MEM_NONTEMPORAL | PMEM_F_MEM_NODRAIN);
         _current += Schema::ROW_LENGTH + PMEM_HEADER_SIZE;
     }
 
