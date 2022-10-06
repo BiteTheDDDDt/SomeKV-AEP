a29ad03bbb2f451a7beabeedb8c896c52d74894f
BiteTheDDDDt <pxl290@qq.com>
2022-10-06 18:55:49 +0800

    fix

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,6 +1,5 @@
 #include "interface.h"
 
-#include "storage/storage_engine.h"
 #include "storage/storage_engine_factory.h"
 #include "utils/schema.h"
 
@@ -8,13 +7,13 @@ void engine_write(void* ctx, const void* data, size_t len) {
     if (len != Schema::ROW_LENGTH) {
         LOG(FATAL) << "len!=ROW_LENGTH(len=" << len << ")";
     }
-    auto* engine = static_cast<StorageEngine*>(ctx);
+    auto* engine = static_cast<StorageEngineP2P*>(ctx);
     engine->write(data);
 }
 
 size_t engine_read(void* ctx, int32_t select_column, int32_t where_column, const void* column_key,
                    size_t column_key_len, void* res) {
-    auto* engine = static_cast<StorageEngine*>(ctx);
+    auto* engine = static_cast<StorageEngineP2P*>(ctx);
     return engine->read(select_column, where_column, column_key, column_key_len, res);
 }
 
@@ -32,5 +31,5 @@ void* engine_init(const char* host_info, const char* const* peer_host_info,
 }
 
 void engine_deinit(void* ctx) {
-    delete static_cast<StorageEngine*>(ctx);
+    delete static_cast<StorageEngineP2P*>(ctx);
 }
